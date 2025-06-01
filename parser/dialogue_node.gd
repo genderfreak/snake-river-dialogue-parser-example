092188d42728d6cb9e9a6f00fc718e79f15f0577
Cassie <cjlane204@gmail.com>
2025-06-01 00:11:35 -0600

    docs 2

--- parser/dialogue_node.gd
+++ parser/dialogue_node.gd
@@ -2,25 +2,29 @@ extends RefCounted
 
 class_name DialogueNode
 
+## A class used to represent a DialogueNode in Snake River Dialogue Editor. Has a dictionary, fields,
+## and outputs, as well as some other data.
+
+## The dictionary the DialogueNode represents (outputs, fields, etc.)
 var dictionary: Dictionary
 
-## Create a new DialogueNode with data from _dictionary
-## Does a sanity check to ensure the data is formatted properly
+## Create a new DialogueNode with data from _dictionary.
+## Does a sanity check to ensure the data is formatted properly.
 func _init(_dictionary: Dictionary):
 	if _dictionary.is_empty(): push_error("Tried to create DialogueNode with empty dictionary!")
 	if not _dictionary.has("fields"): push_error("Tried to create DialogueNode with dictionary with no fields key!")
 	if not _dictionary.has("outputs"): push_error("Tried to create DialogueNode with dictionary with no outputs key!")
 	dictionary = _dictionary
 
-## Returns the fields dict
+## Returns the fields dict.
 func get_fields() -> Dictionary:
 	return dictionary["fields"]
 
-## Returns the outputs array
+## Returns the outputs array.
 func get_outputs() -> Array:
 	return dictionary["outputs"]
 
-## Returns the field specified or false if it doesn't exist
+## Returns the field specified or false if it doesn't exist.
 func get_field(field: StringName):
 	if dictionary["fields"].has(field):
 		return dictionary["fields"][field]
